770e942c486d289c6a32345ff9a5eadf55ebaadb
Arnaud Prat <arnaud.prat@gmail.com>
2015-09-06 16:36:33 +0200

    Add timeline-nav

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+  // circle-nav functions
   $('.circle-nav__item a').hover(function(){
     $(this).siblings('.tooltip').toggleClass('is_open');
   });
@@ -9,6 +10,25 @@ $(document).ready(function() {
     setTimeout(function() {
       $('.circle-nav__item').eq(i).show().addClass('bounceIn');
     }, 200 * i);
+  });
+
+  // timeline-nav functions
+  $('.timeline-nav__item').hide();
+  $('.timeline-nav__item').each(function(i) {
+    setTimeout(function() {
+      $('.timeline-nav__item').eq(i).show().addClass('bounceIn');
+    }, 200 * i);
+    setTimeout(function() {
+      $('.bubble').first().addClass('is_open')
+    }, 1200);
+  });
+
+  $('.timeline-nav__item a').on('click', function() {
+    event.preventDefault();
+
+    $('.timeline-nav__item').find('.bubble').removeClass('is_open');
+
+    $(this).next().addClass('is_open');
   });
 
 });
