93a3a6f796f91df838850f56483f973f9fde8087
Arnaud Prat <arnaud.prat@gmail.com>
2015-09-06 11:44:37 +0200

    Add loading animation

--- assets/js/function.js
+++ assets/js/function.js
@@ -3,5 +3,12 @@ $(document).ready(function() {
   $('.circle-nav__item a').hover(function(){
     $(this).siblings('.tooltip').toggleClass('is_open');
   });
-  
+
+  $('.circle-nav__item').hide();
+  $('.circle-nav__item').each(function(i) {
+    setTimeout(function() {
+      $('.circle-nav__item').eq(i).show().addClass('bounceIn');
+    }, 200 * i);
+  });
+
 });
